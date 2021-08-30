3afb82bd474bc9e7fe07ca273003a479767b25a2
nameless <nameless@NAMELESS-PC>
2021-08-30 20:29:48 -0300

    Add project files.

---
+++ DS_PLUS_COMPILER/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DS_PLUS_COMPILER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
